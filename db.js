d2eb1035f13528edc757ea395ee441513c787404
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-10-30 14:30:00 +0530

    Update urlRoute.js

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ async function getDb() {
 const uri = "mongodb+srv://pasanpiyumantha98_db_user:EJHYVA4XOAqtOTNk@cluster0.kzo6buv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
+// Connecting to the MongoDB server
 await client.connect();
 const db = client.db("RankReminder");
 
