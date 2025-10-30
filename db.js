7ff1d695853ba2a650a0a7f312bbf12881670ff3
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-10-30 14:30:00 +0530

    Update urlRoute.js

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 import { MongoClient } from 'mongodb';
 
+// Function to get the database connection
 async function getDb() {
 
 const uri = "mongodb+srv://pasanpiyumantha98_db_user:EJHYVA4XOAqtOTNk@cluster0.kzo6buv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
